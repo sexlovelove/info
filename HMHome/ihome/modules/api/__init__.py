8a03748737560a15dfc7c95d340b2ba255a3a2cc
李建芬 <manager@qq.com>
2019-03-19 13:54:14 +0800

    删除误操作

--- HMHome/ihome/modules/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-# 创建 api_1_0 接口的蓝图
-api_blu = Blueprint("api", __name__, url_prefix="/api/v1.0")
-
-from . import passport, profile, house, order
--- HMHome/ihome/modules/html/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# 1. 创建蓝图
-html_blu = Blueprint("html", __name__)
-
-# 4. 关联视图函数(避免循环导入)
-from .views import *
